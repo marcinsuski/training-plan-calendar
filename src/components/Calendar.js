29354ff9463e4360554d9c71c81ecd46ab328db5
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-12-03 11:26:42 +0100

    added calendar frame

---
+++ src/components/Calendar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Calendar.module.css';
+
+const Calendar = () => {
+  return (
+    <div className={classes.calendar}>Calendar</div>
+  )
+  
+}
+
+export default Calendar
