e6176b26b513052f2a59f44c090e888680e16cd6
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-02-01 16:40:01 +0100

    added big-calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,89 @@
 import "./App.css";
-import Calendar from './components/Calendar';
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const locales = {
+    "en-US": require("date-fns/locale/en-US"),
+};
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+
+const events = [
+    {
+        title: "Big Meeting",
+        allDay: true,
+        start: new Date(2023, 1, 2),
+        end: new Date(2023, 1, 2),
+    },
+    {
+        title: "Vacation",
+        allDay: true,
+        start: new Date(2023, 1, 1),
+        end: new Date(2023, 1, 1),
+    },
+    {
+        title: "Conference",
+        start: new Date(2023, 1, 5),
+        end: new Date(2023, 1, 5),
+    },
+];
 
 function App() {
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+    const [allEvents, setAllEvents] = useState(events);
+
+    const handleAddEvent = () => {
+        setAllEvents([...allEvents, newEvent]);
+    };
+
     return (
         <div className="App">
-            <Calendar />{" "}
+            <h1>Calendar</h1>
+            <h2>Add New Event</h2>
+            <div style={{ width: "100%" }}>
+                <input
+                    type="text"
+                    placeholder="Add Title"
+                    style={{ width: "20%", marginRight: "10px" }}
+                    value={newEvent.title}
+                    onChange={(e) =>
+                        setNewEvent({ ...newEvent, title: e.target.value })
+                    }
+                />
+                <DatePicker
+                    placeholderText="Start Date"
+                    style={{ marginRight: "10px" }}
+                    selected={newEvent.start}
+                    onChange={(start) => setNewEvent({ ...newEvent, start })}
+                />
+                <DatePicker
+                    placeholderText="End Date"
+                    selected={newEvent.end}
+                    onChange={(end) => setNewEvent({ ...newEvent, end })}
+                />
+                <button style={{ marginTop: "10px" }} onClick={handleAddEvent}>
+                    Submit
+                </button>
+            </div>
+            <Calendar
+                localizer={localizer}
+                events={allEvents}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 600, width: "80%", margin: "50px" }}
+            />
         </div>
     );
 }
--- src/components/CalendarGrid.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CalendarGrid = () => {
-  return (
-    <div>CalendarGrid</div>
-  )
-}
-
-export default CalendarGrid
